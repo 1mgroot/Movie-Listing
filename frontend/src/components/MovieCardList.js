6242cf60c98fbef827d1b27a7586589c5a75b356
1mgroot <40841493+1mgroot@users.noreply.github.com>
2023-02-28 16:48:58 -0500

    create basic movie card and movie list component

--- frontend/src/components/MovieCardList.js
+++ frontend/src/components/MovieCardList.js
@@ -1,12 +1,13 @@
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from "react"
 import MovieCard from "./MovieCard";
 
 export default function MovieCardList({movies}) {
 
     return (
-        <div>
-            {movies.map(movie=>(<MovieCard movie={movie} key={movie.id}/>))}
-        </div>
+        <Grid2 container spacing={3}>
+            {movies.map(movie=>(<Grid2 xs={3} key={movie.id}><MovieCard movie={movie}/></Grid2>))}
+        </Grid2>
 
     );
 }
