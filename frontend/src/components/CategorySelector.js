331081af86da6940b215b0ce32f650a68bfb5295
1mgroot <40841493+1mgroot@users.noreply.github.com>
2023-03-01 18:25:37 -0500

    code cleanup

--- frontend/src/components/CategorySelector.js
+++ frontend/src/components/CategorySelector.js
@@ -22,20 +22,3 @@ export default function CategorySelector({category, handleCategoryChange}) {
         </FormControl>
     );
 }
-
-
-// export function CategorySelector(){
-//     return (
-//         <Select
-//         labelId="demo-simple-select-label"
-//         id="demo-simple-select"
-//         // value={age}
-//         label="Category"
-//         // onChange={handleChange}
-//       >
-//         <MenuItem value={10}>Ten</MenuItem>
-//         <MenuItem value={20}>Twenty</MenuItem>
-//         <MenuItem value={30}>Thirty</MenuItem>
-//       </Select>
-//     )
-// }
