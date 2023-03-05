1c6f1b3c6f8fbeb354c3882b21a8688d58eb073a
1mgroot <40841493+1mgroot@users.noreply.github.com>
2023-03-04 22:07:54 -0500

    6 movie details page (#7)
    * Got data from API
    
    * Add moive detail page, refactor getImgFullUrl to helpers
    
    * code clean up
    
    * set up react-router-dom, able to link from movie card title to movie detail

---
+++ frontend/src/pages/MovieDetail.js
@@ -0,0 +1,72 @@
+import { Chip, Typography } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import React, { useEffect, useState } from "react";
+import { fetchMoviesDetailByID } from "../utils/fetchFromAPI";
+import { getImgFullUrl } from "../utils/helpers";
+import StarIcon from '@mui/icons-material/Star';
+import { useParams } from "react-router-dom";
+
+
+export function ProductionCompany({ name, logo_path }) {
+    return (
+        <Grid2>
+            <Typography variant="body1">{name}</Typography>
+            <img style={{ height: '30px' }} src={getImgFullUrl(logo_path)} />
+        </Grid2>
+
+    );
+}
+
+
+export default function MovieDetail() {
+    const [movieData, setMoviesData] = useState({});
+    const {movieID} = useParams();
+
+    useEffect(() => {
+        fetchMoviesDetailByID(movieID).then((data) => {
+            setMoviesData(data);
+        })
+    }, [movieID]);
+
+    return (
+        <Grid2 container spacing={2} justifyContent="center">
+            <Grid2 xs={9} container>
+                <Grid2 xs={6}>
+                    <img style={{width:"100%"}} src={getImgFullUrl(movieData.poster_path)}/>
+                </Grid2>
+                <Grid2 container xs={6}>
+                    <Grid2 xs={12}>
+                        <Typography variant="h3">{movieData.title}</Typography>
+                        <Typography variant="h6">Release Date:</Typography>
+                        <Typography variant="body1">{movieData.release_date}</Typography>
+                        <Typography variant="h6">Overview:</Typography>
+                        <Typography variant="body1">{movieData.overview}</Typography>
+                        <Typography variant="h6">Genres:</Typography>
+                        <Grid2 container>
+                        {movieData.genres?.map((genre)=>{
+                            return (
+                                <Grid2 key={genre.id}>
+                                    <Chip  color="primary" label={genre.name}/>
+                                </Grid2>
+                            )
+                        })}
+                        </Grid2>
+                        <Typography variant="h6">Average Rating:</Typography>
+                        <Grid2 container>
+                            <Grid2><StarIcon style={{color:"#F5C518"}}/></Grid2>
+                            <Grid2><Typography variant="body1">{movieData.vote_average}</Typography></Grid2>
+                        </Grid2>
+                        
+                        <Typography variant="h6">Production Companies:</Typography>
+                        <Grid2 container>
+                            {movieData.production_companies?.map((company) => {
+                                return <ProductionCompany key={company.id} name={company.name} logo_path={company.logo_path} />
+                            })}
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
+            </Grid2>
+
+        </Grid2>
+    )
+}
--- frontend/src/utils/fetchFromAPI.js
+++ frontend/src/utils/fetchFromAPI.js
@@ -1,9 +1,18 @@
 const API_KEY = `9496cf73fa27b7ee8376e145cb4f69dc`;
-
+const URL_PREFIX = `https://api.themoviedb.org/3/movie`
 export const fetchMoviesByCategory = (category, page) => {
-  const url = `https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}&page=${page}`;
+  const url = `${URL_PREFIX}/${category}?api_key=${API_KEY}&page=${page}`;
 
   return fetch(url).then((resp) => {
     return resp.json();
   });
 };
+
+export const fetchMoviesDetailByID = (movie_id) => {
+    // https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+    const url = `${URL_PREFIX}/${movie_id}?api_key=${API_KEY}&language=en-US`;
+  
+    return fetch(url).then((resp) => {
+      return resp.json();
+    });
+  };
